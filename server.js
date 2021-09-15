b5fd1891ac3f71946750e0ca23e95e57470476e8
Saurabh <saurabhshivam4@gmail.com>
2021-09-15 14:11:47 +0530

    Connected to the database for feedback form

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -11,11 +12,45 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+
+// Creating mongodb server with the help of mongoose
+mongoose.connect("mongodb://localhost:27017/cvFeedbackDB", {
+    useNewUrlParser: true
+});
+
+// Creating mongoose schema
+const feedbackSchema = {
+    Name: String,
+    Email: String,
+    Message: String
+};
+
+// Creating new model
+const Feedback = mongoose.model("Feedback", feedbackSchema);
+
 app.get('/', (req, res) => {
 
     res.sendFile(__dirname + "/index.html");
 
 });
+
+app.post('/', (req, res) => {
+
+    // Creating new document 
+    const feedback = new Feedback({
+        Name: req.body.name,
+        Email: req.body.email,
+        Message: req.body.messg
+    });
+
+    // Saving the document to the database
+    feedback.save(function (err) {
+        if (!err) {
+            res.redirect("/");
+        }
+    });
+
+})
 
 app.listen(3000, function () {
     console.log("Serveris running on port 3000");
